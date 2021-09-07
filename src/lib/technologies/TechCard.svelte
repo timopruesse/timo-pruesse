09189d55a0feb3dc84f333fa929d4619c9342fa7
Timo Prüße <chroma91@gmail.com>
2021-09-08 00:33:54 +0200

    animation/transform

--- src/lib/technologies/TechCard.svelte
+++ src/lib/technologies/TechCard.svelte
@@ -4,7 +4,7 @@
 	export let technology: Technology;
 </script>
 
-<a href={technology.url} target="_blank" rel="noopener noreferrer" class="px-2 pt-1 pb-2 bg-white text-gray-900 shadow rounded-lg overflow-hidden hover:bg-gray-700 hover:text-white cursor-pointer">
+<a href={technology.url} target="_blank" rel="noopener noreferrer" class="hover:z-50 transition duration-400 ease-in-out hover:transform hover:-skew-y-1 px-2 pt-1 pb-2 bg-white text-gray-900 shadow rounded-lg overflow-hidden hover:bg-gray-700 hover:text-white cursor-pointer">
 	<div class="mt-1 text-2xl font-medium">
 		{technology.name}
 	</div>
