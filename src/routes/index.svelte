bbe54990712d5be155efae72292a06929583beda
Timo Prüße <chroma91@gmail.com>
2021-09-26 23:56:53 +0200

    remove unneeded import

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <script context="module">
-	import { browser, dev } from '$app/env';
+	import { browser } from '$app/env';
 	import { headerStore } from '$lib/header/store';
 	import TechGrid from '$lib/technologies/TechGrid.svelte';
 
