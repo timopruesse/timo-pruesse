5d70d22a1948f3ffe9a4a39e646692cc6e75071b
Timo Prüße <chroma91@gmail.com>
2022-01-27 16:45:31 +0100

    add custom scroll bar

--- src/lib/terminal/Terminal.svelte
+++ src/lib/terminal/Terminal.svelte
@@ -25,6 +25,7 @@
 
 <div
 	bind:this={terminal}
+	id="terminal"
 	class={`text-iceblue bg-nightowl p-4 sm:p-8 font-mono text-sm sm:text-base md:text-xl max-h-[80vh] md:max-h-[67vh] overflow-y-auto overflow-x-auto md:overflow-x-hidden ${
 		$$props.class || ''
 	}`.trim()}
@@ -37,3 +38,30 @@
 		<TerminalInput bind:instance={currentLine} />
 	{/if}
 </div>
+
+<style lang="scss">
+	#terminal {
+		&::-webkit-scrollbar {
+			@media (max-width: 640px) {
+				width: 10px;
+			}
+			width: 4px;
+		}
+
+		&::-webkit-scrollbar-track {
+			background-color: transparent;
+		}
+
+		&::-webkit-scrollbar-thumb {
+			background: rgba($color: #043254, $alpha: 0.67);
+
+			@media (min-width: 641px) {
+				border-radius: 0.75rem;
+			}
+		}
+
+		&::-webkit-scrollbar-thumb:hover {
+			background: rgba($color: #043254, $alpha: 1);
+		}
+	}
+</style>
